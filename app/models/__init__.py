03a4be0622a7cb6371d5f90dba2a80271372f39f
Ashmita001 <ashmita.202004@gmail.com>
2025-06-25 20:06:52 +0530

    user_dashboard

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,9 @@ from .admin import Admin
 from .parking_lot import ParkingLot
 from .parking_spot import ParkingSpot
 from .reservation import Reservation
+
+
+from app.models.user import User
+from app.models.parking_lot import ParkingLot
+from app.models.parking_spot import ParkingSpot
+from app.models.parking_record import ParkingRecord
